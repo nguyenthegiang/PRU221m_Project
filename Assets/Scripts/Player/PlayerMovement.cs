abff9d059b7036b4fa24e0c406fb491643c1b338
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-06-22 17:27:10 +0700

    Create Main Character:

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //movement speed
+    [SerializeField]
+    public float Speed = 10f;
+
+    //for movement
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
