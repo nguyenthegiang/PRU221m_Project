c71441fbde01d356cbe6aef67d597c4f40647c6b
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-06-27 17:29:42 +0700

    Add movement for Main Character

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //for movement
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+    [SerializeField]
+    private Rigidbody2D rb;
     //movement speed
     [SerializeField]
     public float Speed = 10f;
 
-    //for movement
-    float horizontalMove = 0f;
-    float verticalMove = 0f;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //get input movement
+        verticalMove = Input.GetAxisRaw("Vertical");
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        //move character
+        rb.velocity = new Vector2(horizontalMove * Speed, verticalMove * Speed);
     }
 }
