5cd156acddf402783c7ef62d1eedb7cd45445963
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-07-07 23:33:01 +0700

    [Main Character Animation Transition]-{State Pattern}: Add StateContext

---
+++ Assets/Scripts/Player/MovementAnimation/MoveStateContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Context class, use to get current state and change state
+/// </summary>
+class MoveStateContext
+{
+    //current state of movement
+    public IMoveState CurrentState { get; set; }
+
+    // controller to get and/or set properties of movement
+    private readonly MoveController _moveController;
+
+    //constructor
+    public MoveStateContext(MoveController moveController)
+    {
+        //instantiate the controller
+        _moveController = moveController;
+    }
+
+    /// <summary>
+    /// Change to new state and call handle method of that state
+    /// </summary>
+    /// <param name="state">the new state to change to</param>
+    public void Transition(IMoveState state)
+    {
+        CurrentState = state;
+        CurrentState.Handle(_moveController);
+    }
+}
