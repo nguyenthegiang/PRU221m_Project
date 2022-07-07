e51646219f6e2ab0c90f9a7d6771a4c474e2934c
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-07-07 23:39:47 +0700

    [Main Character Animation Transition]-{State Pattern}: Add StateController

--- Assets/Scripts/Player/MovementAnimation/MoveController.cs
+++ Assets/Scripts/Player/MovementAnimation/MoveController.cs
@@ -16,15 +16,50 @@ public class MoveController : MonoBehaviour
     //for States to change Sprite of GameObject
     public SpriteRenderer SpriteRenderer { get; set; }
 
+    // states of movement
+    private IMoveState _moveDownState, _moveUpState, _moveLeftState, _moveRightState;
+
+    //context class
+    private MoveStateContext _moveStateContext;
+
     void Awake()
     {
-        //instantiate
-        SpriteRenderer = GetComponent<SpriteRenderer>();        
+        //instantiate spriteRenderer
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        //init state context and move states
+        _moveStateContext = new MoveStateContext(this);
+        //assign move states to gameObject
+        _moveDownState = gameObject.AddComponent<MoveDownState>();
+        _moveUpState = gameObject.AddComponent<MoveUpState>();
+        _moveLeftState = gameObject.AddComponent<MoveLeftState>();
+        _moveRightState = gameObject.AddComponent<MoveRightState>();
+
+        //set default state
+        _moveStateContext.Transition(_moveRightState);
     }
 
-    // Update is called once per frame
-    void Update()
+    //change to move up animation
+    public void MoveUpAnimation()
     {
-        
+        _moveStateContext.Transition(_moveUpState);
+    }
+
+    //change to move down animation
+    public void MoveDownAnimation()
+    {
+        _moveStateContext.Transition(_moveDownState);
+    }
+
+    //change to move left animation
+    public void MoveLeftAnimation()
+    {
+        _moveStateContext.Transition(_moveLeftState);
+    }
+
+    //change to move right animation
+    public void MoveRightAnimation()
+    {
+        _moveStateContext.Transition(_moveRightState);
     }
 }
