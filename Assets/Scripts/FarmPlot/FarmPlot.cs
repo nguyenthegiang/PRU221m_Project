4c2343cf8fb56536a2017c8473e8ac434c9711fa
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-07-17 16:42:57 +0700

    [State Pattern - Farm]: State Client

--- Assets/Scripts/FarmPlot/FarmPlot.cs
+++ Assets/Scripts/FarmPlot/FarmPlot.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// [State Deisgn Pattern - Client: Farm]
+/// Control the Farm Plot;
+/// For Farmer to call
+/// </summary>
 public class FarmPlot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //For controlling Farm State of Farm Plot
+    private FarmController _farmController;
+
     void Start()
     {
-        
+        //instantiate farm controller
+        _farmController = GetComponent<FarmController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //(For Farmer to call) [Client] call to method of Controller to change state
+    public void FarmSeed()
     {
-        
+        _farmController.FarmSeed();
+    }
+
+    //(For Farmer to call) [Client] call to method of Controller to change state
+    public void FarmWater()
+    {
+        _farmController.FarmWater();
+    }
+
+    //(For this class to call) [Client] call to method of Controller to change state
+    public void FarmRipe()
+    {
+        _farmController.FarmRipe();
+    }
+
+    //(For Farmer to call) [Client] call to method of Controller to change state
+    public void FarmHarvest()
+    {
+        _farmController.FarmHarvest();
     }
 }
